f1dedaf2944e54b9fa64edd67f016cb369fbd5ce
Aleksandr Zhegalo <zhegalo00@list.ru>
2023-02-19 12:33:29 +0300

    save

---
+++ Sem3Task17/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача №17
+// Напишите программу, которая принимает на вход
+// координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0
+// и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+//<тип> <имя метода>(<тип> <название переменной>) 
+//{
+  //  return 
+//}
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+void PrintQuterTest(int x, int y)
+{
+if(x>0&&y>0)Console.WriteLine("Точка в 1 четверти");
+if(x>0&&y<0)Console.WriteLine("Точка в 2 четверти");
+if(x<0&&y<0)Console.WriteLine("Точка в 3 четверти");
+if(x<0&&y>0)Console.WriteLine("Точка в 4 четверти");
+}
+
+int coordX = ReadData("Введите координату X");
+int coordY = ReadData("Введите координату Y");
