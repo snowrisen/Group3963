fcc90ad489a92068bf7a4f0857c6e51f99c430fc
Aleksandr Zhegalo <zhegalo00@list.ru>
2023-02-25 15:36:27 +0300

    sem3

--- Sem1Task1/Program.cs
+++ Sem1Task1/Program.cs
@@ -6,21 +6,21 @@
 Console.Write("Введите число №1: ");
 string numLine1 = Console.ReadLine();
 //ввод числа №2
-Console.Write("Введите число №2: ");
-string numLine2 = Console.ReadLine();
+ string numLine2 = Console.ReadLine();
 
-if(numLine1!=null && numLine2!=null)
+ if(numLine1!=null && numLine2!=null)
 {
 //Convert to int
-int num1 = int.Parse(numLine1);
-int num2 = int.Parse(numLine2); 
-if(num2*num2 == num1) 
-             {       
-Console.WriteLine("Да, является!");
+ int num1 = int.Parse(numLine1);
+ int num2 = int.Parse(numLine2); 
+ if(num2*num2 == num1) 
+            //  {       
+ Console.WriteLine("Да, является!");
 
-   }   
-   else   
-{       
-Console.WriteLine("Нет, не является!");   
- }
- }
+    }   
+    else   
+ {       
+ Console.WriteLine("Нет, не является!");   
+  }
+
+
---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,59 @@
+﻿//Напишите программу, которая принимает на вход
+//пятизначное число и проверяет, является ли оно палиндромом.
+
+//14212 -> нет
+
+//12821 -> да
+
+//23432 -> да
+
+
+ // Создаем словарь для четырехзначных палиндромов
+        Dictionary<int, bool> palindromes = new Dictionary<int, bool>();
+
+        // Заполняем словарь четырехзначными палиндромами
+        for (int i = 1001; i < 10000; i++)
+        {
+            string str = i.ToString();
+            if (str[0] == str[3] && str[1] == str[2])
+            {
+                palindromes[i] = true;
+            }
+        }
+
+        // Считываем пятизначное число 
+        Console.Write("Введите пятизначное число: ");
+        int inputNumber = int.Parse(Console.ReadLine());
+
+        // Проверяем, является ли введенное число палиндромом
+        bool isPalindrome = false;
+        if (inputNumber >= 10000 && inputNumber <= 99999)
+        {
+            string inputString = inputNumber.ToString();
+            char[] charArray = inputString.ToCharArray();
+            Array.Reverse(charArray);
+            string reverseString = new string(charArray);
+            isPalindrome = inputString == reverseString;
+        }
+
+        // Проверяем, является ли введенное число четырехзначным палиндромом
+        bool isFourDigitPalindrome = false;
+        if (inputNumber >= 1000 && inputNumber <= 9999)
+        {
+            isFourDigitPalindrome = palindromes.ContainsKey(inputNumber);
+        }
+
+        // Выводим результат
+        if (isPalindrome)
+        {
+            Console.WriteLine($"{inputNumber} является пятизначным палиндромом!");
+        }
+        else if (isFourDigitPalindrome)
+        {
+            Console.WriteLine($"{inputNumber} является четырехзначным палиндромом!");
+        }
+        else
+        {
+            Console.WriteLine($"{inputNumber} не является палиндромом!");
+        }
+ 
